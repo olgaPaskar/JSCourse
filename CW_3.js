b8659400fb4aa00d6f6e7fb58eb579dec6b9f63d
Olga <110352047+olgaPaskar@users.noreply.github.com>
2024-02-13 12:32:57 +0200

    CW_3.js

--- CW_3.js
+++ CW_3.js
@@ -1,16 +1,17 @@
 //Напишем функцию getTimeOfDay, которая принимает текущий час (от 0 до 23) и возвращает строку,
 // указывающую на время суток: утро, день, вечер или ночь.
 
-const getTimeOfDay = (getHourse) => {
-    if (getHourse >= 6 && getHourse <= 11) {
+const getTimeOfDay = (hourse) => {
+    if (hourse >= 6 && hourse <= 11) {
         return 'morning';
-    } else if (getHourse > 11 && getHourse <= 16) {
+    } else if (hourse > 11 && hourse <= 16) {
         return 'day';
-    } else if (getHourse > 16 && getHourse <= 22) {
+    } else if (hourse > 16 && hourse <= 22) {
         return 'evening';
     } else {
         return 'night'
     }
+}
 // - Написать программу, используя цикл for, которая выводит в консоль числа от 1 до 10.
 // - Написать программу, используя цикл for, которая выводит числа от 1 до 10, прерывая выполнение при достижении числа 7.
 // - Используя цикл while, вывести все четные числа от 1 до 20, пропуская числа, кратные 5.
@@ -20,28 +21,24 @@ const getTimeOfDay = (getHourse) => {
         console.log(i);
     }
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 10; i++) {
         if (i === 7)
             break;
         console.log(i)
     }
-    let i = 2;
+    let i = 0;
     while (i <= 20) {
-        if (i % 5 == 0)
-            console.log(i);
+        if (i % 2 === 0 && i % 5 != 0 )
+    console.log(i)
         i++
     }
-}
+
 
     /*Создать функцию formatPhoneNumber, которая принимает строку, представляющую номер телефона, и возвращает отформатированную строку.
     formatPhoneNumber("1234567890");
      Вывод: "(123) 456-7890"*/
 
-const phoneNumber = "1234567890";
-    formatPhoneNumber = (phoneNumber) => {
-        return phoneNumber.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
-    }
-    console.log(formatPhoneNumber())
-
-
-
+const formatPhoneNumber = num => {
+    return num.replace(/(...)(...)(.*)/, '($1) $2-$3')
+}
+console.log(formatPhoneNumber('1234567890'))
